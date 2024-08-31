db6e919929d45fa6cadc662c6486e1fa9ba36ec6
Ankit Kumar Thakur <103780825+DrThakur@users.noreply.github.com>
2024-08-31 21:20:57 +0530

    manullay
    some randome files

---
+++ app.js
@@ -0,0 +1,22 @@
+/********************JAVASCRIPT MASTERY*******************************/
+/********************VARIABLES IN JAVASCRIPT *******************************/
+
+let username; //VARIABLE DECLARATION
+username= "Ankit"; //VARIABLE INTIALISATION
+
+/*WE CAN ALSO DECLARE VARIABLE AND INTIALISE IT IN THE SAME LINE*/
+let name = "Abhsihek"; // VARIBLE DECLARATION AND INTIALISATION SIMULATNEOUSLY
+
+// console.log(name);
+
+username="Mark";
+console.log(username);
+
+const PI=3.14;
+
+/*
+1.When we use const keyword to create a varaible it basically creates a constant whose value cannot be changed throughout the program.
+2.When we declare a varible using const keyword , we must intialise the value of varible in the same line or we should say simultaneously. We can't intialise varibles value declared using const keyword in the next line otherwise it wil throw error.
+
+These restrictions are only for varible declared using const keyword and not on varibles declared using let keyword.
+*/
